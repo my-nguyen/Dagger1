d7d91b3350f5f489504b36e8193b5993e61031a6
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-64G-2T.local>
2023-07-01 15:09:44 -0700

    b3 (amend): added build.gradle dependency

--- app/src/main/java/com/nguyen/dagger1/di/AppComponent.kt
+++ app/src/main/java/com/nguyen/dagger1/di/AppComponent.kt
@@ -3,6 +3,9 @@ package com.nguyen.dagger1.di
 import com.nguyen.dagger1.registration.RegistrationActivity
 import dagger.Component
 
+// if we build the app now, we'll get this error:
+// [Dagger/MissingBinding] com.nguyen.dagger1.storage.Storage cannot be provided without an @Provides-annotated method.
+// that's because we haven't told Dagger how to provide an object of type Storage which is needed by UserManager!
 @Component
 interface AppComponent {
     // RegistrationActivity requests injection; Dagger has to provide the dependencies annotated
