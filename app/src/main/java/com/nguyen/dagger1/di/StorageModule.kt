e0355c0ec742b4b2fceaa1bb076294cbd07f00e5
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-64G-2T.local>
2023-07-02 16:59:10 -0700

    b12: @Provides annotation and Qualifiers - doesn't compile

--- app/src/main/java/com/nguyen/dagger1/di/StorageModule.kt
+++ app/src/main/java/com/nguyen/dagger1/di/StorageModule.kt
@@ -1,17 +1,21 @@
 package com.nguyen.dagger1.di
 
+import android.content.Context
 import com.nguyen.dagger1.storage.SharedPreferencesStorage
 import com.nguyen.dagger1.storage.Storage
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 
 // Modules tell Dagger how to provide instances of a certain type.
 @Module
 abstract class StorageModule {
-    // because Storage is an interface, we need to tell Dagger what implementation of Storage to use
-    // @Binds tells Dagger to use SharedPreferencesStorage as implementation for Storage
-    // SharedPreferencesStorage requires Context which is provided by @BindInstance in AppComponent
-    // factory create method
-    @Binds
-    abstract fun provideStorage(storage: SharedPreferencesStorage): Storage
+    // Apart from the @Inject and @Binds annotations, you can use @Provides to tell Dagger how to
+    // provide an instance of a class inside a Dagger module
+    // use the @Provides annotation in Dagger modules to tell Dagger how to provide classes that
+    // your project doesn't own (e.g. instances of Retrofit).
+    @Provides
+    fun provideStorage(context: Context): Storage {
+        return SharedPreferencesStorage(context)
+    }
 }
